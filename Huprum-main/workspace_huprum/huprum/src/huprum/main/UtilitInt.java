891277563529c757b6347bfb4ac79bcb1f8baf8e
PRlZRAK <82274870+PRlZRAK@users.noreply.github.com>
2021-05-06 23:15:27 +0300

    Delete Huprum-main directory

--- Huprum-main/workspace_huprum/huprum/src/huprum/main/UtilitInt.java
+++
@@ -1,8 +0,0 @@
-package huprum.main;
-
-public interface UtilitInt
-{
-	public int ChecLogin(String login);
-
-	public String ClearPhone(String phone);
-}
--- Huprum-main/workspace_huprum/huprum/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module huprum {
-	exports huprum.main.chat.panels;
-	exports huprum.main.loginer;
-	exports huprum.main;
-	exports huprum.main.chat;
-
-	requires java.desktop;
-	requires org.json;
-}
