021588b6b0962b7773b0eb8f04431fcaa722ddf8
PRlZRAK <alesharodygin@gmail.com>
2021-06-15 11:26:26 +0300

    добавил сообщение о том что не найден пользователь

--- workspace_huprum/huprum/src/huprum/main/toolbar/AddUserActionList.java
+++ workspace_huprum/huprum/src/huprum/main/toolbar/AddUserActionList.java
@@ -22,54 +22,65 @@ public class AddUserActionList implements ActionListener {
 	private Huprum main;
 	private String myId;
 	private HashMap<String, String> pars;
-	public AddUserActionList(Huprum main)
-	{
-		this.main=main;
-		cl=main.getCl();
+
+	public AddUserActionList(Huprum main) {
+		this.main = main;
+		cl = main.getCl();
 		loginer = main.getLoginer();
 		myId = Integer.toString(loginer.getId());
 		pars = new HashMap<String, String>();
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		AddUsOptonPane();
+	}
+
+	public void AddUsOptonPane() {
+
 		String user = JOptionPane.showInputDialog(null, "Введите логин/почту/телефон того, кого хотите добавить",
 				"Добавление переписки", JOptionPane.PLAIN_MESSAGE);
-		int i = Utilit.CheckLogin(user);
-		if(i==1) 
-		{
-			user=Utilit.CleaPhone(user);
+		if (user == null) {
+			System.out.println("Отменено");
+			return;
 		}
-		String[] par = new String[] {"mail","phone","login"};
+		System.out.println("Подтверждено");
+		int i = Utilit.CheckLogin(user);
+		if (i == 1) {
+			user = Utilit.CleaPhone(user);
+		}
+		String[] par = new String[] { "mail", "phone", "login" };
 		pars.clear();
 		pars.put("action", "login");
-		pars.put(par[i],user);
+		pars.put(par[i], user);
 		String otvet;
-		try
-		{
+		try {
 			otvet = cl.send(pars);
 			System.out.println(otvet);
 			JSONObject jo = new JSONObject(otvet);
 			int status = jo.getInt("status");
-			if (status!=0)
-			{
+			if (status != 0) {
+				int repeat = JOptionPane.showConfirmDialog(null, "Пользователь не найден, попробовать ещё раз?",
+						"Ошибка", JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE);
+				if (repeat == JOptionPane.YES_OPTION)
+					AddUsOptonPane();
 				return;
 			}
-			
-				
-			String id=Integer.toString(jo.getInt("id"));
+
+			String id = Integer.toString(jo.getInt("id"));
 			pars.put("action", "put_msg");
 			pars.put("id_from", myId);
 			pars.put("id_to", id);
 			pars.put("msg", "Добавлена новая переписка");
-			try	{cl.send(pars);} catch (IOException b){b.printStackTrace();}
-			return;
-			
-		} catch (IOException d)
-		{
+			try {
+				cl.send(pars);
+			} catch (IOException b) {
+				b.printStackTrace();
+			}
+		} catch (IOException d) {
 			d.printStackTrace();
 			return;
 		}
-
 	}
 
 }
