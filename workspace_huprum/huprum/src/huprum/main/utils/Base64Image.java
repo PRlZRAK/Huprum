5f41aafd73d3c51a7e82f0d0e562f2e4826341f3
yaa1952 <yaa19522007@gmail.com>
2021-06-18 20:45:41 +0300

    добавил пример
    запускайте и смотрите

--- workspace_huprum/huprum/src/huprum/main/utils/Base64Image.java
+++ workspace_huprum/huprum/src/huprum/main/utils/Base64Image.java
@@ -1,12 +1,22 @@
 package huprum.main.utils;
 
-
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ * @author yaa Два статических метода для преобразования файла изображения в
+ *         Base64 и обратно
+ *
+ */
 public class Base64Image
 {
 	private static FileInputStream  imageInFile;
@@ -27,5 +37,21 @@ public class Base64Image
 		imageOutFile = new FileOutputStream(pathFile);
 		byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
 		imageOutFile.write(imageByteArray);
+	}
+	public static void main(String[] args)
+	{
+		String buf;
+		try
+		{
+			buf = Base64Image.encoder(new File("logo_right.png"));
+			System.out.println(buf);
+			byte[]        btDataFile = Base64.getDecoder().decode(buf);
+			BufferedImage image      = ImageIO.read(new ByteArrayInputStream(btDataFile));
+			JOptionPane.showMessageDialog(null, "", "Image", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(image));
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
