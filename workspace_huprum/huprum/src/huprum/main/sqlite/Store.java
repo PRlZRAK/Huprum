701f5381da9b7639e8285463c1588cbe8062bdc0
PRlZRAK <alesharodygin@gmail.com>
2021-06-28 00:28:53 +0300

    Store

--- workspace_huprum/huprum/src/huprum/main/sqlite/Store.java
+++ workspace_huprum/huprum/src/huprum/main/sqlite/Store.java
@@ -15,10 +15,16 @@ public class Store
 		Store st = new Store();
 		try
 		{
-			st.put("name","алексей");
-			st.put("fio","**************");
-			String name=st.get("name");
-			System.out.println(name);
+			st.putAva(1, "image1");
+			st.putAva(2, "image223");
+			st.putChatImg(1, "image1");
+			st.putChatImg(45, "image123");	
+			
+			String img = st.get(2,"AvaImg");
+			String img1 = st.get(45,"ChatImg");
+			
+			System.out.println(img);
+			System.out.println(img1);
 		} catch (SQLException e)
 		{
 			// TODO Auto-generated catch block
@@ -26,11 +32,6 @@ public class Store
 		}
 		st.close();
 	}
-
-
-
-
-
 
 	private void close()
 	{
@@ -42,17 +43,13 @@ public class Store
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
-
-
 	private HashMap<String, String> pars;
-	private Connection conn;
+	private Connection              conn;
 
 	public Store()
 	{
-
 		pars = new HashMap<String, String>();
 		//cl = main.getCl();
 		
@@ -61,52 +58,58 @@ public class Store
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection("jdbc:sqlite:SQL.s3db");
 			Statement statmt = conn.createStatement();
-			//statmt.execute("CREATE TABLE if not exists 'Images' ('id' INT, 'base64Img' TEXT, 'status' INT);");
-			statmt.execute("CREATE TABLE if not exists \"set1\" (\n" + 
-					"	\"id\"	INTEGER NOT NULL,\n" + 
-					"	\"key\"	TEXT,\n" + 
-					"	\"value\"	TEXT,\n" + 
-					"	PRIMARY KEY(\"id\" AUTOINCREMENT)\n" + 
+			statmt.execute("CREATE TABLE if not exists AvaImg (" + 
+					"	id	INTEGER NOT NULL," + 
+					"	img	TEXT," + 
+					"	PRIMARY KEY(id)" + 
+					");");
+			Statement statmt1 = conn.createStatement();
+			statmt1.execute("CREATE TABLE if not exists ChatImg (" + 
+					"	id	INTEGER NOT NULL," +  
+					"	img	TEXT" + 
 					");");
 		} catch (ClassNotFoundException | SQLException e)
 		{
 			// TODO  joptionpane
 			e.printStackTrace();
 		}
-		
-		
-	}
-	private void put(String key, String value) throws SQLException
-	{
-		PreparedStatement statmt = conn.prepareStatement("SELECT id FROM \"set1\" WHERE key=?");
-		statmt.setString(1, key);	
-		 ResultSet rs = statmt.executeQuery();
-		 if (rs.next())
-		 {
-			int id = rs.getInt("id");
-			 PreparedStatement statmt1 = conn.prepareStatement("UPDATE 'set1' SET value=? WHERE id=?");
-			 statmt1.setString(1, value);
-			 statmt1.setInt(2, id);
-			 statmt1.executeUpdate();
-		 }
-		 else
-		 {
-			 PreparedStatement statmt2 = conn.prepareStatement("INSERT INTO 'set1'('key',value) VALUES (?,?)");
-			
-			 statmt2.setString(1, key);
-			 statmt2.setString(2, value);
-			 statmt2.execute(); 
-		 }
-		
 	}
 
-	private String get(String key) throws SQLException
+	private void putAva(int id, String img) throws SQLException
 	{
-		PreparedStatement statmt = conn.prepareStatement("SELECT value FROM \"set1\" WHERE key=?");
-		statmt.setString(1, key);	
-		 ResultSet rs = statmt.executeQuery();
-		 if (rs.next())
-				return rs.getString("value");
-		 return null;
+		PreparedStatement statmt = conn.prepareStatement("SELECT * FROM AvaImg WHERE id=?");
+		statmt.setInt(1, id);
+		ResultSet rs = statmt.executeQuery();
+		if (rs.next())
+		{
+			PreparedStatement statmt1 = conn.prepareStatement("UPDATE AvaImg SET img=? WHERE id=?");
+			statmt1.setString(1, img);
+			statmt1.setInt(2, id);
+			statmt1.executeUpdate();
+		} else
+		{
+			PreparedStatement statmt2 = conn.prepareStatement("INSERT INTO AvaImg(id,img) VALUES (?,?)");
+			statmt2.setInt(1, id);
+			statmt2.setString(2, img);
+			statmt2.execute();
+		}
+	}
+	private void putChatImg(int id, String img) throws SQLException
+	{
+			PreparedStatement statmt = conn.prepareStatement("INSERT INTO ChatImg(id,img) VALUES (?,?)");
+			statmt.setInt(1, id);
+			statmt.setString(2, img);
+			statmt.execute();
+	}
+	
+
+	private String get(int id, String table) throws SQLException
+	{
+		PreparedStatement statmt = conn.prepareStatement("SELECT img FROM "+ table +" WHERE id=?");
+		statmt.setInt(1, id);
+		ResultSet rs = statmt.executeQuery();
+		if (rs.next())
+			return rs.getString("img");
+		return null;
 	}
 }
