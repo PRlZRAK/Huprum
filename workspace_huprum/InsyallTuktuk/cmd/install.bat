8fa541264525c3f89570d780987c8624427b7004
PRlZRAK <alesharodygin@gmail.com>
2021-07-18 18:28:04 +0300

    Update install.bat

--- workspace_huprum/InsyallTuktuk/cmd/install.bat
+++ workspace_huprum/InsyallTuktuk/cmd/install.bat
@@ -4,12 +4,14 @@ if %ERRORLEVEL% EQU 0 goto ok
 echo Install jre
 jre-8u291-windows-i586-iftw.exe
 :ok
+chcp 1251
 mkdir "%USERPROFILE%\tuktuk"
 copy tuktuk.jar "%USERPROFILE%\tuktuk"
 copy logo.ico "%USERPROFILE%\tuktuk"
 echo @echo off >  "%USERPROFILE%\tuktuk\tuktuk.bat" 
-echo cd %USERPROFILE%\tuktuk >>  "%USERPROFILE%\tuktuk\tuktuk.bat" 
-echo start javaw -jar tuktuk.jar >>  "%USERPROFILE%\tuktuk\tuktuk.bat"
+echo chcp 1251 >>  "%USERPROFILE%\tuktuk\tuktuk.bat" 
+echo cd "%USERPROFILE%\tuktuk" >>  "%USERPROFILE%\tuktuk\tuktuk.bat" 
+echo start javaw -jar "%USERPROFILE%\tuktuk\tuktuk.jar" >>  "%USERPROFILE%\tuktuk\tuktuk.bat"
 :: --------------
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%USERPROFILE%\Desktop\tuktuk.lnk" >> CreateShortcut.vbs
@@ -20,5 +22,4 @@ echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
 :: --------------
-
 
