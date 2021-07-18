e909fd37825b186b74f31d3948a0268c1ce72128
PRlZRAK <alesharodygin@gmail.com>
2021-07-18 16:04:42 +0300

    Update install.bat

--- workspace_huprum/InsyallTuktuk/cmd/install.bat
+++ workspace_huprum/InsyallTuktuk/cmd/install.bat
@@ -4,21 +4,21 @@ if %ERRORLEVEL% EQU 0 goto ok
 echo Install jre
 jre-8u291-windows-i586-iftw.exe
 :ok
-mkdir %cd:~0,2%\tuktuk
-copy tuktuk.jar %cd:~0,2%\tuktuk
-copy logo.ico %cd:~0,2%\tuktuk
-echo @echo off >  %cd:~0,2%\tuktuk\tuktuk.bat 
-echo cd %cd:~0,2%\tuktuk >>  %cd:~0,2%\tuktuk\tuktuk.bat 
-echo java -jar tuktuk.jar >>  %cd:~0,2%\tuktuk\tuktuk.bat
+mkdir %USERPROFILE%\tuktuk
+copy tuktuk.jar %USERPROFILE%\tuktuk
+copy logo.ico %USERPROFILE%\tuktuk
+echo @echo off >  %USERPROFILE%\tuktuk\tuktuk.bat 
+echo cd %USERPROFILE%\tuktuk >>  %USERPROFILE%\tuktuk\tuktuk.bat 
+echo start javaw -jar tuktuk.jar >>  %USERPROFILE%\tuktuk\tuktuk.bat
 :: --------------
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "..\tuktuk.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%USERPROFILE%\Desktop\tuktuk.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%cd:~0,2%\tuktuk\tuktuk.bat" >> CreateShortcut.vbs
-echo oLink.IconLocation = "%cd:~0,2%\tuktuk\logo.ico" >> CreateShortcut.vbs
+echo oLink.TargetPath = "%USERPROFILE%\tuktuk\tuktuk.bat" >> CreateShortcut.vbs
+echo oLink.IconLocation = "%USERPROFILE%\tuktuk\logo.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
 :: --------------
-pause
+
 
