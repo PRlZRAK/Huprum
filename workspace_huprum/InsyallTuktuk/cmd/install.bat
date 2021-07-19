90f76cecf6f7660d9e7aad913fef0e0d988a167c
PRlZRAK <alesharodygin@gmail.com>
2021-07-19 22:17:46 +0300

    Update install.bat

--- workspace_huprum/InsyallTuktuk/cmd/install.bat
+++ workspace_huprum/InsyallTuktuk/cmd/install.bat
@@ -13,8 +13,11 @@ echo chcp 1251 >>  "%USERPROFILE%\tuktuk\tuktuk.bat"
 echo cd "%USERPROFILE%\tuktuk" >>  "%USERPROFILE%\tuktuk\tuktuk.bat" 
 echo start javaw -jar "%USERPROFILE%\tuktuk\tuktuk.jar" >>  "%USERPROFILE%\tuktuk\tuktuk.bat"
 :: --------------
+set reg_path=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders
+set reg_param=desktop
+for /f "tokens=1,2,*" %%a in ('reg query "%reg_path%" /v "%reg_param%"') do if "%%a"=="%reg_param%" set reg_value=%%c
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%USERPROFILE%\Desktop\tuktuk.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%reg_value%\tuktuk.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%USERPROFILE%\tuktuk\tuktuk.bat" >> CreateShortcut.vbs
 echo oLink.IconLocation = "%USERPROFILE%\tuktuk\logo.ico" >> CreateShortcut.vbs
@@ -22,4 +25,3 @@ echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
 :: --------------
-
