f05f9f4cb5d0480d91e8d43f261f37ee6b3ddac9
yaa1952 <yaa19522007@gmail.com>
2021-08-06 12:05:37 +0300

    Решение Лешиной задачи

---
+++ workspace_huprum/Tuktuk/src/su/tuktuk/utilit/TestDescktop.java
@@ -0,0 +1,21 @@
+package su.tuktuk.utilit;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+public class TestDescktop
+{
+	public static void main(String[] args)
+	{
+		File f=new File("test.3gp");
+		try
+		{
+			Desktop.getDesktop().open(f);
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
