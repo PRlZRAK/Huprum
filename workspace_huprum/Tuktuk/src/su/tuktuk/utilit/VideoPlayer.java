e7f65dddc6186f48ffcf82bfe371a929c7f003d9
PRlZRAK <alesharodygin@gmail.com>
2021-09-20 14:20:04 +0300

    что сделал
    видимо что то не так. какой юрл я бы не давал не работает.

--- workspace_huprum/Tuktuk/src/su/tuktuk/utilit/VideoPlayer.java
+++ workspace_huprum/Tuktuk/src/su/tuktuk/utilit/VideoPlayer.java
@@ -1,25 +1,45 @@
 package su.tuktuk.utilit;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class VideoPlayer
 {
-	public VideoPlayer(URL url)
+	private File f;
+
+	public VideoPlayer(URL url) throws IOException
 	{
-		// TODO Auto-generated constructor stub
+		f=File.createTempFile("test", ".3gp");			
+		Path path = f.toPath();
+		try (InputStream in = url.openStream()) {
+		    Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
+		}
+		f.deleteOnExit();
 	}
 
 	private void play()
 	{
-		// TODO Auto-generated method stub
+		try
+		{
+			Desktop.getDesktop().open(f);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		try
 		{
-			VideoPlayer p = new VideoPlayer(new URL(null));
+			VideoPlayer p = new VideoPlayer(new URL("https://cloud.mail.ru/public/5xDX/pJUojZADX"));
 			p.play();
 		} catch (MalformedURLException e)
 		{
