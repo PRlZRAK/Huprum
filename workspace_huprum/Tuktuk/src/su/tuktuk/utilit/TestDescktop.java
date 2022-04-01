0ebed19a9bb9499c60053629169138eb723bfaf8
yaa1952 <yaa19522007@gmail.com>
2022-04-01 12:41:40 +0300

    1

--- workspace_huprum/Tuktuk/src/su/tuktuk/utilit/TestDescktop.java
+++ workspace_huprum/Tuktuk/src/su/tuktuk/utilit/TestDescktop.java
@@ -8,7 +8,8 @@ public class TestDescktop
 {
 	public static void main(String[] args)
 	{
-		File f=new File("long_test.3gp");
+		//File f=new File("long_test.3gp");
+		File f=new File("http://tuktuk.su/huprum/server/img/long_test");
 		try
 		{
 			Desktop.getDesktop().open(f);
