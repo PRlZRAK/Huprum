dfe1ab0c343d618b547e13684fa84286de4e9089
yaa1952 <yaa19522007@gmail.com>
2022-04-07 20:28:29 +0300

    Update MStor.java

--- workspace_huprum/Tuktuk/src/su/tuktuk/utilit/MStor.java
+++ workspace_huprum/Tuktuk/src/su/tuktuk/utilit/MStor.java
@@ -3,31 +3,38 @@ package su.tuktuk.utilit;
 import java.io.File;
 import java.util.HashMap;
 
-
-public class MStor extends HashMap<String, File> {
-
+public class MStor extends HashMap<String, File>
+{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void add(String key, File med) {
+	public void add(String key, File med)
+	{
 		put(key, med);
 	}
 
-	public File get1(String key) {
-   if(containsKey(key)) return get(key);
-   else return null;
+	public File get1(String key)
+	{
+		if (containsKey(key))
+			return get(key);
+		else
+			return null;
 	}
-	public boolean has(String key) {
-		   if(containsKey(key)) return true;
-		   else return false;
-			}
 
-	public static void main(String[] args) {
+	public boolean has(String key)
+	{
+		if (containsKey(key))
+			return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args)
+	{
 		MStor s = new MStor();
 		s.add("1", null);
-        System.out.print(s.get1("fdsa"));
+		System.out.print(s.get1("fdsa"));
 	}
-
 }
